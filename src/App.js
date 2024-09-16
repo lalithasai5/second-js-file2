f9583e1853fc5ef3246b18417e78bdc2c8c0946f
lalitha sai <lalitha4525@gmail.com>
2024-09-17 00:48:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        image
+      </h1>
+      <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/beautiful-nature-mountain-scenery-with-flowers-free-photo.jpg?w=600&quality=80"></img>
+      <p>Nature is the ultimate source of our living. Both living and non-living things include nature, and everyone is interdependent, which helps maintain the ecosystem. Plants, animals, and humans all depend on nature for their survival. It supplies oxygen, sunlight, soil, water, and other necessary components.
+      </p>
+      <form>
+        <div>
+        <label htmlFor="name">name</label>
+        <input id="name"></input>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
